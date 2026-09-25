0300b5ae2da66335be01c7369793ae6dc7e4098e
Yowlry <toprogram20250314@outlook.com>
2026-09-25 13:41:38 +0800

    Update function_numguess.c
    呼んだ？

--- function_numguess.c
+++ function_numguess.c
@@ -5,34 +5,28 @@
 
 int main()
 {
-	/*
-		生成1 ~ 100之间的随机数，使用键盘录入去猜，猜中为止
-	*/
-
-	// 1. 生成1 ~ 100之间的随机数
 	srand(time(NULL));
 
 	int rand_number_game = rand() % 100 + 1;
 
-	// 2. 利用循环+键盘录入去猜
 	int guess;
 
 	while (1)
 	{
-		printf("%s\n", "请输入您要猜的数字：");
+		printf("%s\n", "please enter a number to guess: ");
 		scanf("%d", &guess);
 
 		if (guess < rand_number_game)
 		{
-			printf("%s\n", "猜小了");
+			printf("%s\n", "too low");
 		}
 		else if (guess > rand_number_game)
 		{
-			printf("%s\n", "猜大了");
+			printf("%s\n", "too high");
 		}
 		else
 		{
-			printf("%s\n", "呼んだ？");
+			printf("%s\n", "鍛笺倱銇狅紵");
 			break;
 		}
 	}
