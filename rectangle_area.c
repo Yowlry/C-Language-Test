7fb0b81a77df25051babd8b27f49bddc7e32a860
Yowlry <toprogram20250314@outlook.com>
2026-09-25 16:59:55 +0800

    Rectangle Area
    use functions & if-else logic (if statements) to determine rectangle areas

--- rectangle_area.c
+++ rectangle_area.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // 给定两个矩形，使用函数判断谁的面积更大
+// Given two rectangles, use a function to determine which one has a larger area
 double getArea1(double len, double width)
 {
 	double area = len * width;
@@ -11,21 +12,23 @@ double getArea1(double len, double width)
 int main5()
 {
 	// 1. 调用函数去求矩形的面积
+	// 1. Call a function to calculate the area of the rectangle
 	double area1 = getArea1(number1, number2);
 	double area2 = getArea1(number3, number4);
 
 	// 2. 判断
+	// 2. Determine
 	if (area1 > area2)
 	{
-		printf("%s\n", "第一个矩形更大");
+		printf("%s\n", "the first rectangle is larger");
 	}
 	else if (area2 > area1)
 	{
-		printf("%s\n", "第二个矩形更大");
+		printf("%s\n", "The second rectangle is larger");
 	}
 	else
 	{
-		printf("%s\n", "两个矩形一样大");
+		printf("%s\n", "both rectangles are equal in size");
 	}
 
 
