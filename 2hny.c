ab35e923d07b4da92e46c204dee4e4c88435baea
Yowlry <toprogram20250314@outlook.com>
2026-09-26 07:02:19 +0800

    Happy New Year
    print 'Happy New Year!' in C using two output methods: direct printf output and defined string variable output

---
+++ 2hny.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main6()
+{
+    printf("%s\n", "Happy New Year!");
+
+    char congratulation[16] = "Happy New Year!";
+    printf("%s\n", congratulation);
+
+    return 0;
+}
