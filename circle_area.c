63a3f0ea567db4e4e641f81901cff527035b2e48
Yowlry <toprogram20250314@outlook.com>
2026-09-25 17:07:44 +0800

    Circle Area
    use functions & if-else logic (if statements) to determine circle areas

---
+++ circle_area.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// 给定两个圆形，使用函数判断哪个圆的面积更大
+// Given two circles, use a function to determine which one has a larger area
+double getCircle(double r)
+{
+	double radius;
+}
+
+
+int main()
+{
+	double c_r1 = getCircle(number1);
+	double c_r2 = getCircle(number2);
+
+	if (c_r1 > c_r2)
+	{
+		printf("%s\n", "the first circle is larger");
+	}
+	else if (c_r2 > c_r1)
+	{
+		printf("%s\n", "the second circle is larger");
+	}
+	else
+	{
+		printf("%s\n", "both circles are equal in size");
+	}
+
+
+	return 0;
+}
